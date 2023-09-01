6b4429bf1f9730de45ae3b8736399b94f6ab7134
Roberto Cortez <radcortez@yahoo.com>
2023-09-01 13:02:09 +0100

    Reproduce quarkus-35410

--- src/main/java/com/radcortez/quarkus/config/ConfigResource.java
+++ src/main/java/com/radcortez/quarkus/config/ConfigResource.java
@@ -5,16 +5,28 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @Path("/config")
 public class ConfigResource {
     @Inject
     SmallRyeConfig config;
+    @ConfigProperty(name = "foo")
+    String foo;
 
     @GET
     @Path("/{name}")
     public Response get(@PathParam("name") String name) {
         return Response.ok().entity(config.getConfigValue(name)).build();
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/prop")
+    public String helloProp() {
+        return foo;
     }
 }
