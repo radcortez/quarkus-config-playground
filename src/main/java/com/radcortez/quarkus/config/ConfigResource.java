04f64d23ae56a5d8b69c0c871c19211bbf606f01
Roberto Cortez <radcortez@yahoo.com>
2023-10-04 13:58:55 +0100

    Reproduce quarkus-36199

--- src/main/java/com/radcortez/quarkus/config/ConfigResource.java
+++ src/main/java/com/radcortez/quarkus/config/ConfigResource.java
@@ -1,5 +1,7 @@
 package com.radcortez.quarkus.config;
 
+import io.quarkus.agroal.runtime.DataSourceJdbcRuntimeConfig;
+import io.quarkus.agroal.runtime.DataSourcesJdbcRuntimeConfig;
 import io.smallrye.config.SmallRyeConfig;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -7,14 +9,22 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.Response;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @Path("/config")
 public class ConfigResource {
     @Inject
     SmallRyeConfig config;
+    @Inject
+    DataSource dataSource;
+    @Inject
+    DataSourcesJdbcRuntimeConfig dataSourcesJdbcRuntimeConfig;
 
     @GET
     @Path("/{name}")
-    public Response get(@PathParam("name") String name) {
+    public Response get(@PathParam("name") String name) throws Exception {
+        final Connection connection = dataSource.getConnection();
         return Response.ok().entity(config.getConfigValue(name)).build();
     }
 }
