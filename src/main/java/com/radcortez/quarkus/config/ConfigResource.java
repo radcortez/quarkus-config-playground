1bd21d9f008de9bbdc8b2b05c2c1c1706de47682
Roberto Cortez <radcortez@yahoo.com>
2023-12-01 19:52:20 +0000

    Reproduce quarkus-37306

--- src/main/java/com/radcortez/quarkus/config/ConfigResource.java
+++ src/main/java/com/radcortez/quarkus/config/ConfigResource.java
@@ -1,5 +1,6 @@
 package com.radcortez.quarkus.config;
 
+import io.quarkus.redis.datasource.RedisDataSource;
 import io.smallrye.config.SmallRyeConfig;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
@@ -11,10 +12,15 @@ import jakarta.ws.rs.core.Response;
 public class ConfigResource {
     @Inject
     SmallRyeConfig config;
+    @Inject
+    RedisDataSource redisDataSource;
 
     @GET
     @Path("/{name}")
     public Response get(@PathParam("name") String name) {
+        System.out.println(config.getConfigValue("quarkus.redis.password"));
+        System.out.println(config.getConfigValue("AUTH_CONSENT_REDIS_PASSWORD"));
+        System.out.println(config.getConfigValue("auth.consent.redis.password"));
         return Response.ok().entity(config.getConfigValue(name)).build();
     }
 }
